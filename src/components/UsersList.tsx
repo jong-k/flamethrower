b3dbc826a7eaf7f8a1ef8fe0fa06cc0fe70a0ba7
jong-k <kjhan710@naver.com>
2023-02-04 08:57:18 +0900

    feat: json-server 에 요청 기능 추가

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
-  return <div>Hello from UsersList</div>;
+  const dispatch = useAppDispatch();
+  const { isLoading, data, error } = useAppSelector((state) => {
+    return state.users; // { data, [], isLoading: false, error: null }
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error != null) return <div>Error fetching data...</div>;
+
+  return <div>{data.length}</div>;
 };
 
 export default UsersList;
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,3 +6,9 @@ export const store = configureStore({
     users: usersReducer,
   },
 });
+
+// store에서 모든 것을 export 해주기 위해서
+// fetchUsers.ts 에서 export 해주는것 + 여기서 export 하는것 까지 export
+export * from "./thunks/fetchUsers";
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
