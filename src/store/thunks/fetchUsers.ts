af197df90facbe7ed436af4ca3e66715882dda3c
jong-k <kjhan710@naver.com>
2023-02-04 09:06:22 +0900

    feat: loading test용 pause 기능 추가

--- src/store/thunks/fetchUsers.ts
+++ src/store/thunks/fetchUsers.ts
@@ -4,8 +4,16 @@ const USERS_URL = "http://localhost:3005/users";
 
 export const fetchUsers = createAsyncThunk("users/fetch", async () => {
   const response = await fetch(USERS_URL);
+  await pause(1000); // TODO - 끝나면 지우기!
   return await response.json();
 });
+
+// fetching 의도적으로 늦추는 함수 -> test용
+const pause = async (duration: number) => {
+  return await new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 /*
 RTK의 createAsyncThunk를 사용하면 다음 action type이 자동으로 추가된다
